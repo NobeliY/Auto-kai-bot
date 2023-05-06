5b4323e76a66304f64dc3887ef27fd1fe8110466
NobeliY <nobeliylord@gmail.com>
2023-05-06 20:24:18 +0300

    Added FSM States save on PostgreSQL Database!

--- Telegram/app.py
+++ Telegram/app.py
@@ -1,3 +1,4 @@
+import asyncio
 import logging
 from logging import StreamHandler, FileHandler
 
@@ -5,11 +6,14 @@ import sys
 from colorama import Fore
 from aiogram import Bot, Dispatcher, executor
 from Data import BOT_TOKEN, LOGGING_FILE, check_log_dir, LOGGING_PATH
-from Data.config import LOGGING_LEVEL
-from states.loader import storage
+from Data.config import LOGGING_LEVEL, PG_USER, PG_PASSWORD, PG_DATABASE
+from utils.database_api.database_gino import database
+from utils.storage.storages import PGStorage
 
-bot = Bot(BOT_TOKEN, parse_mode="HTML")
-dp = Dispatcher(bot=bot, storage=storage)
+bot: Bot = Bot(BOT_TOKEN, parse_mode="HTML")
+
+storage: PGStorage = PGStorage(username=PG_USER, password=PG_PASSWORD, db_name=PG_DATABASE)
+dp: Dispatcher = Dispatcher(bot=bot, storage=storage)
 logging_level = LOGGING_LEVEL
 logging.basicConfig(level=logging_level,
                     format=f"{Fore.YELLOW}%(asctime)s {Fore.RESET}| [{Fore.CYAN}%(levelname)s"
@@ -21,5 +25,5 @@ logging.getLogger().addHandler(FileHandler(filename=f"{LOGGING_PATH}/{LOGGING_FI
 # TODO: Main
 if __name__ == "__main__":
     from Handler.default.default import get_default_commands
-
+    logging.warning(f"{dp}")
     executor.start_polling(dp, skip_updates=True, on_startup=get_default_commands)
